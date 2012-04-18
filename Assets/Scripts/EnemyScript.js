50f656766ce0574bad9161ef9a26e9de69272711
Greg Graham <Greg Graham>
2012-04-18 09:06:57 -0500

    Configured build settings, and fixed end of game enemy respawn.

--- Assets/Scripts/EnemyScript.js
+++ Assets/Scripts/EnemyScript.js
@@ -7,9 +7,9 @@ function Update () {
 	var amtToMove = EnemySpeed * Time.deltaTime;
 	transform.Translate(Vector3.down * amtToMove);
 	
-	// If the enemy is at the bottom of the screen, deduct from the
-	// score for letting it get by. Then "respawn" the enemy.
-	if (transform.position.y <= -5.2) {
+	// If the enemy is at the bottom of the screen and the game is not over,
+	// deduct from the score for letting it get by and "respawn" the enemy.
+	if (transform.position.y <= -5.2 && GameScript.Lives > 0) {
 		GameScript.Score -= 20;
 		GameScript.Respawn(transform);
 	}
